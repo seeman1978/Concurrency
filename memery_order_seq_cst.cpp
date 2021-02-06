8e67c3d03c8419c92389568983ef9e8d9eaa2ea1
wangqiang <seeman1978@163.com>
2021-02-06 16:06:16 +0800

    排序一致序列

---
+++ memery_order_seq_cst.cpp
@@ -0,0 +1,46 @@
+//
+// Created by 王强 on 2021/2/6.
+//
+
+#include <atomic>
+#include <thread>
+#include <cassert>
+#include <iostream>
+std::atomic<bool> x,y;
+std::atomic<int> z;
+void write_x()
+{
+    x.store(true,std::memory_order_seq_cst);  // 1
+}
+void write_y()
+{
+    y.store(true,std::memory_order_seq_cst);  // 2
+}
+void read_x_then_y()
+{
+    while(!x.load(std::memory_order_seq_cst));
+    if(y.load(std::memory_order_seq_cst))  // 3
+        ++z;
+}
+void read_y_then_x()
+{
+    while(!y.load(std::memory_order_seq_cst));
+    if(x.load(std::memory_order_seq_cst))  // 4
+        ++z;
+}
+int main()
+{
+    x=false;
+    y=false;
+    z=0;
+    std::thread a(write_x);
+    std::thread b(write_y);
+    std::thread c(read_x_then_y);
+    std::thread d(read_y_then_x);
+    a.join();
+    b.join();
+    c.join();
+    d.join();
+    assert(z.load()!=0);  // 5
+    std::cout << " z is " << z << "\n";
+}
