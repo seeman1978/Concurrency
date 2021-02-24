478f3895b891220602b54ceceefda18c1e235e9e
wangqiang <seeman1978@163.com>
2021-02-24 17:26:02 +0800

    atomic_flag实现自旋锁

---
+++ spinlock_mutex.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 王强 on 2021/2/24.
+//
+
+#include <atomic>
+
+class spinlock_mutex
+{
+    std::atomic_flag flag;
+public:
+    spinlock_mutex():
+            flag(ATOMIC_FLAG_INIT)
+    {}
+    void lock()
+    {
+        while(flag.test_and_set(std::memory_order_acquire));
+    }
+    void unlock()
+    {
+        flag.clear(std::memory_order_release);
+    }
+};
